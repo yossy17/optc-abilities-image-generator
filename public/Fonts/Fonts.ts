b2d29900f179747618f0ead8c4824b99124227ee
Yos_sy <hiitoy0117@gmail.com>
2024-08-11 16:31:44 +0900

    commit

--- public/Fonts/Fonts.ts
+++ public/Fonts/Fonts.ts
@@ -1,10 +1,6 @@
-// import { Noto_Sans_JP, Poppins, Inconsolata } from "next/font/google";
 import localFont from "next/font/local";
 
 // カスタムローカルフォントを定義する
 export const PixelMplus10Regular = localFont({
   src: "./PixelMplus10-Regular.ttf",
-});
-export const PixelMplus12Regular = localFont({
-  src: "./PixelMplus12-Regular.ttf",
 });
